68281e470fd0a9b7722132b8090fa1c19aa1c829
DanGrund <hello@dangrund.com>
2017-01-21 17:56:29 -0700

    add App component, vid 10

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -6,10 +6,12 @@ const Main = React.createClass({
     return (
       <div>
         <h1>
-          <Link to="/">Reduxtagram</Link>
+          <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     )
   }
 })
+
 export default Main
